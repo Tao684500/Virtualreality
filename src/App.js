c45851541c1dca2741c11c732cedec5afcf4a05d
Nuttawut <nuttawut@orisma.com>
2025-03-25 15:27:32 +0700

    add package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import MoviePage from './page/MoviePage'; 
+import TodoList from './page/TodoList';
+import "./scss/home.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='home'>
+        <nav className='nav'>
+          <ul>
+            <li>
+              <NavLink 
+                className='link' 
+                to="/movie" 
+                style={({ isActive }) => ({
+                  textDecoration: isActive ? 'underline' : 'none', // ขีดเส้นใต้เมื่อลิงค์เป็นหน้า active
+                  color: isActive ? 'rgb(255, 75, 75)' : 'rgb(12, 52, 253)' // เปลี่ยนสีเมื่อ active
+                })}
+              >
+                Go to Movie Page
+              </NavLink>
+              <NavLink 
+                className='link' 
+                to="/todo-list" 
+                style={({ isActive }) => ({
+                  textDecoration: isActive ? 'underline' : 'none', 
+                  color: isActive ? 'rgb(255, 75, 75)' : 'rgb(12, 52, 253)'
+                })}
+              >
+                Go to Todo
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/movie" element={<MoviePage />} />
+          <Route path="/todo-list" element={<TodoList />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
